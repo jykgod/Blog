8c474d14b76028c2cecd1e9f08f9b7e7d82daae9
jyk <513303276@qq.com>
2016-01-19 15:38:15 +0800

    canvas
    try drag the Canvas out from Panel;Maybe the panel's defined in JAVA is
    better than it in NGUI

--- ui/src/BaseLib/Canvas.js
+++ ui/src/BaseLib/Canvas.js
@@ -1,9 +1,29 @@
-function Canvas(canvas){
-    this.canvas  = canvas;
-    if(canvas == null) return ;
-    var bbox = canvas.getBoundingClientRect();
-    this.x = bbox.left * (canvas.width / bbox.width);
-    this.y = bbox.top * (canvas.height / bbox.height);
-    this.w = canvas.width;
-    this.h = canvas.height;
+//使用html的canvas进行构造
+// function Canvas(canvas){
+//     this.canvas  = canvas;
+//     if(canvas == null) return ;
+//     var bbox = canvas.getBoundingClientRect();
+//     this.x = bbox.left * (canvas.width / bbox.width);
+//     this.y = bbox.top * (canvas.height / bbox.height);
+//     this.w = canvas.width;
+//     this.h = canvas.height;
+// }
+//2016.1.19	尝试把canvas和panel定义分开？
+function Canvas(){
+	this.x = 0;
+	this.y = 0;
+	this.w = 1;
+	this.h = 1;
+	this.canvas;
+}
+Canvas.prototype = {
+	createWithFatherName : function(fatherName)
+	{
+		var fatherNode = document.getElementById(fatherName);
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = this.w;
+		this.canvas.height = this.h;
+		this.canvas.x = this.x;
+		this.canvas.y = this.y;
+	}
 }

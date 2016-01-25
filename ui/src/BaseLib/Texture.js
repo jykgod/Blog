49ecaec4ee9b38acc9b05bee16b3e7517651cdde
jyk <513303276@qq.com>
2016-01-25 18:22:04 +0800

    complete the function draw in the texture

--- ui/src/BaseLib/Texture.js
+++ ui/src/BaseLib/Texture.js
@@ -6,18 +6,18 @@ function Texture()
     DrawableObject.call( this );
     this.src = null;
     this.img = null;
-    this.D
-    this.drawType = 0;
+    this.clipRect = null;
 }
 
 for (var i in DrawableObject.prototype){
     Texture.prototype[i] = DrawableObject.prototype[i];
 }
 
-Texture.prototype.createWith2Factor = function( canvas , src )
+Texture.prototype.createWith2Factor = function( canvas , src ,clipRect)
 {
     this.create(canvas);
     this.src = src;
+    this.clipRect = clipRect;
 }
 
 Texture.prototype.loadTexture = function()
@@ -32,18 +32,22 @@ Texture.prototype.fixSizeAsTextureSize = function()
     this.setSize(this.img.width,this.img.height);
 }
 
-Texture.prototype.drawWith = function()
+Texture.prototype.drawTexture = function()
 {
     if ( this.src == null || this.canvas == null) return;
     var fatherPosition = this.getFatherPosition();
     var x = this.x + fatherPosition.x;
     var y = this.y + fatherPosition.y;
-    this.canvas.mContext.drawImage(this.img , this.x, this.y , this.w , this.h );
+    if(this.clipRect == null) {
+        this.canvas.mContext.drawImage(this.img, x, y, this.w, this.h);
+    }else {
+        this.canvas.mContext.drawImage(this.img, this.clipRect.x, this.clipRect.y, this.clipRect.w, this.clipRect.h, x, y, this.w, this.h);
+    }
 }
 Texture.prototype.draw = function()
 {
     var tempAlpha = this.canvas.mContext.globalAlpha;
     this.canvas.mContext.globalAlpha = this.mAlpha;
-
+    this.drawTexture();
     this.canvas.mContext.globalAlpha = tempAlpha;
 }

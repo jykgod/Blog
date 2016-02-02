57cee33b56ca966309c2446383ec6ca815801052
jyk <513303276@qq.com>
2016-02-02 15:37:24 +0800

    complete the DragPanel

--- ui/src/BaseLib/DragPanel.js
+++ ui/src/BaseLib/DragPanel.js
@@ -1,129 +1,79 @@
-//function DragPanel(canvas){
-//    BaseObject.call(this);
-//    Panel.call(this,canvas);
-//    this.x = 0;
-//    this.y = 0;
-//    this.w = this.mCanvas.w;
-//    this.h = this.mCanvas.h;
-//    this.initialPosition = new Object();
-//    this.offset = new Object();
-//    this.offset.x = 0;
-//    this.offset.y = 0;
-//    this.mVertical = true;
-//    this.mHorizontal = false;
-//    this.mLayoutType = this.mHorizontal;
-//    addEventFunc(this);
-//}
-//
-//for (var i in BaseObject.prototype){
-//    DragPanel.prototype[i] = BaseObject.prototype[i];
-//}
-//
-//for (var i in Panel.prototype){
-//    DragPanel.prototype[i] = Panel.prototype[i];
-//}
-//
-//DragPanel.prototype.draw = function(){
-//    if ( this.getIfChange() == false) return;
-//    var ctx=this.getCanvas().canvas.getContext("2d");
-//    if(this.mTransparent) ctx.alpha= this.alpha;
-//    /*-----------------------clear the mRect---------------------------*/
-//    if(this.mTransparent) {
-//            ctx.clearRect(this.x, this.y, this.x + this.w, this.y + this.h);
-//    }
-//    /*----------------------draw the background--------------------------*/
-//    if(this.isImageBackGround == false){
-//        ctx.fillStyle = this.color;
-//        ctx.fillRect(this.x,this.y,this.x+this.w,this.y+this.h);
-//    }else{
-//        this.drawImage();
-//    }
-//    /*---------------------draw the object in it-------------------------*/
-//    var now = this.objectListHead;
-//    while(now.next != null){
-//        now = now.next;
-//        if ( this.mLayoutType == this.mHorizontal ) now.data.x = now.data.x + this.offset.x;
-//        else now.data.y = now.data.y + this.offset.y;
-//        now.data.draw();
-//    }
-//    this.offset.x = 0;
-//    this.offset.y = 0;
-//    this.setChange(false);
-//}
-//
-//DragPanel.prototype.reSort =function(delta){
-//    var now = this.objectListHead;
-//    var nowtemp = 0;
-//    while(now.next != null){
-//        now = now.next;
-//        if ( this.mLayoutType == this.mHorizontal ) {
-//            now.data.x = nowtemp;
-//            nowtemp += now.data.w;
-//        }
-//        else {
-//            now.data.y = nowtemp;
-//            nowtemp += now.data.h;
-//        }
-//        nowtemp += delta;
-//    }
-//}
-//
-//DragPanel.prototype.setType = function(ttype){
-//    this.mLayoutType = ttype;
-//    this.setChange(true);
-//}
-//
-//DragPanel.prototype.onMouseMove = function(x,y){
-//    var now = this.objectListHead;
-//    while (now.next != null) {
-//            now = now.next;
-//            if(now.data.pointerOnIt(x, y))
-//                now.data.onMouseMove(x,y);
-//    }
-//    if(this.isPressDown){
-//        this.offset.x = x - this.initialPosition.x;
-//        this.offset.y = y - this.initialPosition.y;
-//        this.initialPosition.x = x;
-//        this.initialPosition.y = y;
-//        this.setChange(true);
-//    }
-//}
-//
-//DragPanel.prototype.onClick = function(x,y){
-//    var now = this.objectListHead;
-//    while(now.next != null){
-//        now = now.next;
-//        now.data.onClick(x,y);
-//        if(now.data.getIfChange()) this.setChange(true);
-//    }
-//}
-//
-//DragPanel.prototype.onMouseDown = function(x,y){
-//    if(this.mPointerOn) {
-//        this.isPressDown = true;
-//        this.initialPosition.x = x;
-//        this.initialPosition.y = y;
-//        var now = this.objectListHead;
-//        while(now.next != null){
-//            now = now.next;
-//            now.data.onMouseDown(x,y);
-//            if(now.data.getIfChange()) this.setChange(true);
-//        }
-//    }
-//}
-//
-//DragPanel.prototype.onMouseUp = function(x,y){
-//    var now = this.objectListHead;
-//    while(now.next != null){
-//        now = now.next;
-//        now.data.onMouseUp(x,y);
-//        if(now.data.getIfChange()) this.setChange(true);
-//    }
-//    this.isPressDown = false;
-//}
-//
-///*-------------all objects on the DragPanel will response the event flow the DragPanel response event func------------*/
-//DragPanel.prototype.addObject = function(object){
-//        object.setCanvas(this.mCanvas.canvas);
-//        addObjectInList( this.objectListHead, object);
-//}
+function DragPanel() {
+    Panel.call(this);
+    this.mCollider;
+    this.onPressed = false;
+    this.pointerDownPoint = new function () {
+        this.x = 0;
+        this.y = 0;
+    }
+    this.mAxisXMove = false;
+    this.mAxisYMove = false;
+    this.dragFixX = 0;
+    this.dragFixY = 0;
+}
+
+for (var i in Panel.prototype) {
+    DragPanel.prototype[i] = Panel.prototype[i];
+}
+
+DragPanel.prototype.create = function (canvas) {
+    Panel.prototype.create.call(this, canvas);
+    this.mCollider = new Collider();
+    this.mCollider.create(this, 0);
+    var temp = this;
+    this.mCollider.onMouseDown = function (x, y) {
+        temp.onPressed = true;
+        temp.onMouseDown(x, y);
+    }
+    this.mCollider.onClick = function (x, y) {
+        temp.onClick(x, y);
+    }
+    this.mCollider.onMouseMove = function (x, y) {
+        temp.onMouseMove(x, y);
+    }
+    this.mCollider.onMouseUp = function (x, y) {
+        temp.onMouseUp(x, y);
+    }
+    this.mCollider.onRelease = function (x, y) {
+        temp.onPressed = false;
+        temp.onRelease(x, y);
+    }
+}
+
+DragPanel.prototype.setSize(w, h)
+{
+    Panel.prototype.setSize.call(this, w, h);
+    if (this.mCollider != null)
+        this.mCollider.setSize(w, h);
+}
+
+DragPanel.prototype.onMouseDown = function (x, y) {
+    this.pointerDownPoint.x = x;
+    this.pointerDownPoint.y = y;
+}
+
+DragPanel.prototype.onMouseMove = function (x, y) {
+    if (this.onPressed == true) {
+        var resultX = this.x;
+        var resultY = this.y;
+        if (this.mAxisXMove) {
+            var cXMax = 0;
+            var cXMin = -this.w + this.dragFixX;
+            if (cXMin < cXMax) {
+                resultX += ((x - this.pointerDownPoint.x) * 1.0) / 5;
+                if (resultX < cXMin) resultX = cXMin;
+                if (resultX > cXMax) resultX = cXMax;
+            }
+        }
+        if (this.mAxisYMove) {
+            var cYMax = 0;
+            var cYMin = -this.h + this.dragFixY;
+            if (cYMin < cYMax) {
+                resultY += ((y - this.pointerDownPoint.y) * 1.0) / 5;
+                if (resultY < cYMin) resultY = cYMin;
+                if (resultY > cYMax) resultY = cYMax;
+            }
+        }
+        this.setPosition(resultX, resultY);
+    }
+}
